c920d98485e57fea7397fc0c4a3b3cb55bb0f54c
colemaih <colemaih@mail.uc.edu>
2023-09-14 16:40:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double doubleSalesPrice= 55.5;
+        double doubleSalesTax= doubleSalesPrice * .05;
+        System.out.println("If Your item price is " + doubleSalesPrice + " then your 5% tax would be " +doubleSalesTax );
+
+
+
+    }
+}
